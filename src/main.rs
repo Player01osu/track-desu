5e6dcf54a08f25a4b7db8d0ba35599b029ab7dda
player01osu <player01osu@gmail.com>
2022-12-03 22:11:58 -0500

    Recursive and non-recursive copy and symlinking.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use anyhow::{anyhow, Result};
 use itertools::Itertools;
 use lazy_static::lazy_static;
 use std::{
+    collections::HashSet,
     env,
+    ffi::OsStr,
+    fmt::Display,
     io,
+    mem::size_of_val,
     path::{Path, PathBuf},
 };
 lazy_static! {
-    static ref GIT_DIR: PathBuf = PathBuf::from("/home/bruh/Documents/.tmp/");
+    static ref GIT_DIR: PathBuf = PathBuf::from("/home/bruh/Documents/.tmp/")
+        .canonicalize()
+        .unwrap();
 }
 
 const HELP: &'static str = r#"usage: tracker-desu
@@ -14,7 +21,70 @@ const HELP: &'static str = r#"usage: tracker-desu
 Track dotfiles in a git directory with a single command."#;
 const VERSION: &str = "Tracker-desu: 0.0.0";
 
-fn track_file<T: AsRef<Path>>(path: &T) -> Result<(), io::Error> {
+fn recursive_copy<T: AsRef<Path> + AsRef<OsStr>>(path: &T, to_file: &str) -> Result<(), io::Error> {
+    let path = Path::new(path);
+    if !path.exists() {
+        return Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            "File does not exist",
+        ));
+    }
+
+    fn _recurse(path: &Path, to_file: &str) -> Result<(), io::Error> {
+        if path.is_file() {
+            std::fs::copy(path, &to_file)?;
+        } else {
+            std::fs::create_dir(&to_file)?;
+            for path in path.read_dir()? {
+                let path = path?.path();
+                let to_file = format!("{to_file}/{}", &path.file_name().unwrap().to_string_lossy());
+                _recurse(&path, &to_file)?;
+            }
+        }
+        Ok(())
+    }
+
+    _recurse(path, to_file)?;
+
+    Ok(())
+}
+
+fn track(opts: &HashSet<Opt>, files: Vec<&String>) -> Result<()> {
+    if opts.contains(&Opt::Symlink) {
+        for path in files {
+            symlink_track(path, opts.contains(&Opt::Recursive))?;
+        }
+    } else {
+        for path in files {
+            copy_track(path, opts.contains(&Opt::Recursive))?;
+        }
+    }
+    Ok(())
+}
+
+fn symlink_track<T: AsRef<Path> + AsRef<OsStr> + Display>(
+    path: &T,
+    recursive: bool,
+) -> Result<(), io::Error> {
+    let to_file = format!("{}/{}", GIT_DIR.display(), path);
+    if recursive {
+        recursive_copy(path, &to_file)?;
+        std::fs::remove_file(path).unwrap_or_else(|_| std::fs::remove_dir_all(path).unwrap());
+    } else {
+        std::fs::copy(path, &to_file)?;
+        std::fs::remove_file(path).unwrap();
+    }
+
+    // TODO Check hash before removing
+    std::os::unix::fs::symlink(&to_file, path).unwrap();
+
+    Ok(())
+}
+
+fn copy_track<T: AsRef<Path> + AsRef<OsStr> + Display>(
+    path: &T,
+    recursive: bool,
+) -> Result<(), io::Error> {
     // Copy the file from one directory to the git dir.
     // Compare checksum between two files.
     // Delete file from current path if checksums match.
@@ -22,28 +92,32 @@ fn track_file<T: AsRef<Path>>(path: &T) -> Result<(), io::Error> {
     // match.
     // Create a symlink from the git dir to the
     // given file.
-    let to_file = format!("{}/{}", GIT_DIR.display(), path.as_ref().display());
-    std::fs::copy(path, to_file)?;
+    let to_file = format!("{}/{}", GIT_DIR.display(), &path);
+    if recursive {
+        recursive_copy(path, &to_file)?;
+    } else {
+        std::fs::copy(path, &to_file)?;
+    }
 
     Ok(())
 }
+
+use Argument::*;
 
 #[derive(Clone, Debug, PartialEq, Eq, Ord, PartialOrd)]
 pub enum Argument {
     Help,
     Version,
     Verbose,
+    DryRun,
     Copy,
     Symlink,
     Recursive,
-    DryRun,
     NoConfirm,
-    // TODO
-    GitDir,
+    // TODO No Checksum arg
+    GitDir, // TODO
     File(String),
 }
-
-use Argument::*;
 
 fn short_form_args(args: &str) -> Vec<Result<Argument>> {
     args.chars()
@@ -61,8 +135,6 @@ fn short_form_args(args: &str) -> Vec<Result<Argument>> {
         })
         .collect::<Vec<Result<Argument>>>()
 }
-
-use anyhow::{anyhow, Result};
 
 fn match_args(arg: &str) -> Vec<Result<Argument>> {
     match arg {
@@ -98,7 +170,7 @@ fn match_args(arg: &str) -> Vec<Result<Argument>> {
                 return short_form_args(&s[1..]);
             }
 
-            if &s[0..2] == "--" {
+            if s.len() > 2 && &s[0..2] == "--" {
                 return vec![Err(anyhow!("Invalid argument: {}", s))];
             }
 
@@ -107,8 +179,21 @@ fn match_args(arg: &str) -> Vec<Result<Argument>> {
     }
 }
 
-fn execute_args(args: &[Argument]) -> Result<()> {
-    for arg in args {
+#[derive(Clone, Hash, Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum Opt {
+    NoConfirm,
+    Symlink,
+    Copy,
+    DryRun,
+    Verbose,
+    Recursive,
+}
+
+fn execute_args(args: Vec<Argument>) -> Result<()> {
+    let mut opts = HashSet::new();
+    let mut files = vec![];
+
+    for arg in args.iter() {
         match arg {
             Help => {
                 eprintln!("{HELP}");
@@ -121,12 +206,38 @@ fn execute_args(args: &[Argument]) -> Result<()> {
             GitDir => {
                 unimplemented!()
             }
-            File(ref path) => {
-                track_file(path)?;
+            Verbose => {
+                opts.insert(Opt::Verbose);
             }
-            _ => unimplemented!(),
+            Copy => {
+                if opts.contains(&Opt::Symlink) {
+                    return Err(anyhow!("Copy conflicts with Symlink option"));
+                }
+                opts.insert(Opt::Copy);
+            }
+            Symlink => {
+                if opts.contains(&Opt::Copy) {
+                    return Err(anyhow!("Copy conflicts with Symlink option"));
+                }
+                opts.insert(Opt::Symlink);
+            }
+            DryRun => {
+                opts.insert(Opt::DryRun);
+            }
+            Recursive => {
+                opts.insert(Opt::Recursive);
+            }
+            NoConfirm => {
+                opts.insert(Opt::NoConfirm);
+            }
+            File(ref path) => {
+                files.push(path);
+            }
         }
     }
+
+    track(&opts, files)?;
+
     Ok(())
 }
 
@@ -136,11 +247,10 @@ fn parse_args(args: &[String]) -> Result<()> {
         .dedup()
         .flat_map(|arg| match_args(arg))
         .collect::<Result<Vec<Argument>>>()?;
-    args.sort();
+    args.sort_by(|p, o| o.cmp(p));
 
-    execute_args(&args)?;
+    execute_args(args)?;
 
-    dbg!(args);
     Ok(())
 }
 
